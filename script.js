4b624a744915f50227fa231a20160e62779e4a49
andlo <andlo@outlook.dk>
2021-02-28 10:22:11 +0100

    test

--- script.js
+++ script.js
@@ -111,27 +111,26 @@ $.getJSON("src/points.geojson", function (data){
   }); // insert ".addTo(map)" to display layer by default
   controlLayers.addOverlay(geoJsonLayer, 'Circles');
 });
-// load GeoJSON point data and clickable circles from local directory
-$.getJSON("src/sanitetsstation.geojson", function (data){
-  var geoJsonLayer = L.geoJson(data, {
-    pointToLayer: function( feature, latlng) {
-      var circle = L.circleMarker(latlng, {
-        radius: 8,
-        fillColor: "#ccccff",
-        color: "#0000cc",
-        weight: 2,
-        opacity: 1,
-        fillOpacity: 0.7
-      });
-      circle.bindPopup(feature.properties.Location); // replace 'Location' with properties data label from your GeoJSON file
-      return circle;
-    }
-  }); // insert ".addTo(map)" to display layer by default
-  controlLayers.addOverlay(geoJsonLayer, 'Circles');
-});
+
 
 // load GeoJSON point data and clickable icons from local directory, using jQuery function (symbolized by $)
-$.getJSON("src/points.geojson", function (data){
+//$.getJSON("src/points.geojson", function (data){
+//  var iconStyle = L.icon({
+//    iconUrl: "src/hospital-18.png",
+//    iconRetinaUrl: 'src/hospital-18@2x.png',
+//    iconSize: [18, 18]
+//  });
+ // var geoJsonLayer = L.geoJson(data, {
+ //   pointToLayer: function( feature, latlng) {
+ //     var marker = L.marker(latlng,{icon: iconStyle});
+ //     marker.bindPopup(feature.properties.Location); // replace 'Location' with properties data label from your GeoJSON file
+ //     return marker;
+ //   }
+ // }); // insert ".addTo(map)" to display layer by default
+ // controlLayers.addOverlay(geoJsonLayer, 'Hospitals');
+//});
+
+$.getJSON("src/sanitetsstation.geojson", function (data){
   var iconStyle = L.icon({
     iconUrl: "src/hospital-18.png",
     iconRetinaUrl: 'src/hospital-18@2x.png',
@@ -144,163 +143,8 @@ $.getJSON("src/points.geojson", function (data){
       return marker;
     }
   }); // insert ".addTo(map)" to display layer by default
-  controlLayers.addOverlay(geoJsonLayer, 'Hospitals');
-});
-
-// Flickr photo overlay from remote JSON API feed, such as all Flickr public photos OR only from your account
-// Obtain and insert your own flickr API key
-// https://www.flickr.com/services/apps/create/
-// Use Flickr API explorer to obtain correct endpoint
-// https://www.flickr.com/services/api/explore/?method=flickr.photos.search
-// Example shows photos.search of georeferenced images using keyword tags
-// https://www.flickr.com/services/api/explore/flickr.photos.search
-
-// Define flickrURL endpoint with API explorer: insert your key, and tags= or text= to filter results
-var flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=25dcc9a8c7410551dcb0af48c778bde5&user_id=56513965%40N06&tags=bikemap&extras=geo%2Curl_t%2Curl_s%2Curl_m%2Ctitle&format=json&nojsoncallback=1";
-
-// Define the flickr popup display
-// ** TO DO: Rewrite link to view original source photo directly on Flickr
-// ** POSSIBLY include this code directly in the functions below for easier sequencing by novices
-var popupHTML = function(photo){
-  var result = "";
-      result = '<strong>'+photo.title+'</strong><br>';
-      result += '<a href="'+photo.url_m+'" target="_blank">';
-      result += '<img src="'+photo.url_s+'"></a>';      //was url_t; want url_s; can change to url_m if desired, but frame needs work
-      result += '<br/><small>click image to enlarge in new tab</small>';
-      return result;
-}
-// Load photos from flickr JSON feed (insert your flickrURL above), display with clickable blue markers
-$.getJSON(flickrURL, function (data) {
-  // Create new layerGroup for the markers, with option to append ".addTo(map);" to display by default
-  var layerGroup = new L.LayerGroup();
-  // Add layerGroup to your layer control and insert your label to appear in legend
-  controlLayers.addOverlay(layerGroup, 'Flickr photo blue markers'); // Insert your own legend label
-  // Start a loop to insert flickr photo data into photoContent
-  for (var i = 0; i < data.photos.photo.length; i++) {
-    var photoContent = data.photos.photo[i];
-    var marker = new L.marker([photoContent.latitude, photoContent.longitude]);
-    marker.bindPopup(popupHTML(photoContent));
-    // Add the marker to the layerGroup
-    marker.addTo(layerGroup);
-  }
-});
-
-// Load photos from flickr JSON (insert your flickrURL above), display with clickable photo thumbnails
-$.getJSON(flickrURL, function (data) {
-  // Create new layerGroup for the markers, with option to append ".addTo(map);" to display by default
-  var layerGroup = new L.LayerGroup().addTo(map);
-  // Add layerGroup to your layer control and insert your label to appear in legend
-  controlLayers.addOverlay(layerGroup, 'Flickr photo thumbnail icons');
-  // Start a loop to insert flickr photo data into photoContent
-  for (var i = 0; i < data.photos.photo.length; i++) {
-    var photoContent = data.photos.photo[i];
-    var photoIcon = L.icon(
-      {iconUrl: photoContent.url_t,
-      iconSize: [photoContent.width_t * 0.5, photoContent.height_t * 0.5]}  //reduces thumbnails 50%
-    );
-    var marker = new L.marker([photoContent.latitude, photoContent.longitude], {icon: photoIcon});
-    marker.bindPopup(popupHTML(photoContent));
-    // Add the marker to the layerGroup
-    marker.addTo(layerGroup);
-  }
+  controlLayers.addOverlay(geoJsonLayer, 'Sanitet');
 });
 
 
-/* POLYGON and POLYLINE OVERLAYS */
-// Ways to load geoJSON polygon layers from local directory or remote server
-// Different options for styling and interactivity
 
-$.getJSON("src/lines.geojson", function (data){
-  var geoJsonLayer = L.geoJson(data, {
-    style: function (feature) {
-      return {
-        'color': 'green',
-        'weight': 4,
-      }
-    },
-    onEachFeature: function( feature, layer) {
-      layer.bindPopup(feature.properties.name) // change to match your geojson property labels
-    }
-  });  // insert ".addTo(map)" to display layer by default
-  controlLayers.addOverlay(geoJsonLayer, 'Lines');  // insert your 'Title' to add to legend
-});
-
-$.getJSON("src/parks.geojson", function (data){
-  var geoJsonLayer = L.geoJson(data, {
-    style: function (feature) {
-      return {
-        'color': 'purple',
-        'weight': 2,
-      }
-    },
-    onEachFeature: function( feature, layer) {
-      layer.bindPopup(feature.properties.PARK_NAME) // change to match your geojson property labels
-    }
-  });  // insert ".addTo(map)" to display layer by default
-  controlLayers.addOverlay(geoJsonLayer, 'Parks');  // insert your 'Title' to add to legend
-});
-
-
-// load GeoJSON polyline data
-$.getJSON("src/bus-routes.geojson", function (data){
-  var geoJsonLayer = L.geoJson(data, {
-    style: function (feature) {
-      return {
-        'color': 'red',
-        'weight': 2,
-      }
-    },
-    onEachFeature: function( feature, layer) {
-      layer.bindPopup(feature.properties.LineName) // change to match your geojson property labels
-    }
-  });  // insert ".addTo(map)" to display layer by default
-  controlLayers.addOverlay(geoJsonLayer, 'Bus Routes');  // insert your 'Title' to add to legend
-});
-
-// load polygon data with clickable features from local directory
-$.getJSON("src/polygons.geojson", function (data) {   // insert pathname to your local directory file
-  var geoJsonLayer = L.geoJson(data, {
-    style: function (feature) {
-      return {
-        'color': 'red',
-        'weight': 2,
-        'fillColor': '#fff',
-        'fillOpacity': 0
-      }
-    },
-    onEachFeature: function( feature, layer) {
-      layer.bindPopup(feature.properties.Town) // change 'Town' to match your geojson property labels
-    }
-  });  // insert ".addTo(map)" to display layer by default
-  controlLayers.addOverlay(geoJsonLayer, 'Polygons (CT towns)');  // insert your 'Title' to add to legend
-});
-
-// load polygon geojson, using data to define fillColor, from local directory
-// *TO DO* rebuild file for pop density
-// *TO DO* change from click to hover, and add legend to display colors and hover data
-$.getJSON("src/polygons.geojson", function (data) {   // insert pathname to your local directory file
-  var geoJsonLayer = L.geoJson(data, {
-    style: function (feature) {
-      var fillColor,
-        population = feature.properties.Pop2010;
-      if (population > 100000) fillColor = "#006837";
-      else if (population > 50000) fillColor ="#31a354";
-      else if (population > 15000) fillColor ="#78c679";
-      else if (population > 5000) fillColor ="#c2e699";
-      else if (population > 0) fillColor ="#ffffcc";
-      else fillColor = "#f7f7f7"; // no data
-      return {
-        'color': 'red',
-        'weight': 2,
-        'fillColor': fillColor, // sorts by method above
-        'fillOpacity': 0.8
-      }
-    },
-    onEachFeature: function( feature, layer) {
-      var popupText = "<b>" + feature.properties.Town + "</b>"   // replace labels with those from your own geojson
-         + "<br>Population 2010: " + "<br>" + feature.properties.Pop2010;
-      layer.bindPopup(popupText);
-    }
-  });  // insert ".addTo(map)" to display layer by default
-  controlLayers.addOverlay(geoJsonLayer, 'Polygons filled (CT Pop 2010)');  // insert your 'Title' to add to legend
-});
